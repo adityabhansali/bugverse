cf8c84b8f4224d2b5ef43f24dd9097ec964a7bde
adityabhansali <adityabhansalijain@gmail.com>
2022-09-19 11:43:35 +0530

    template editing

--- database_con.php
+++ database_con.php
@@ -2,7 +2,7 @@
     $servername="localhost";
     $username="root";
     $password="root";
-    $dbname="";
+    $dbname="bugverse";
 
     $conn=new mysqli($servername,$username,$password,$dbname);
 
